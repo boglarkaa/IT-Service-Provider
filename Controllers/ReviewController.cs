74fb3e11e2b29240cba6fb3ea160f8f906418d62
Boglarka Molnar <114649140+boglarkaa@users.noreply.github.com>
2023-01-14 21:24:40 +0200

    Add files via upload

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,43 @@
+﻿using ProiectFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProiectFinal.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private MainDBContext context=new MainDBContext();
+        // GET: Review
+        public ActionResult Index()
+        {
+            return View(context.Reviews.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create() 
+        { 
+            ReviewModel review= new ReviewModel();
+            return View(review);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ReviewModel review)
+        {
+            context.Reviews.Add(review);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Sort() 
+        {
+            var res = from r in context.Reviews
+                      orderby r.NumberOfStars
+                      select r;
+            return View(res);
+        }
+    }
+}
