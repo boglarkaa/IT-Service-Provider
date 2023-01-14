e2f9f04daca3395c8fdbd6c7168f2690be559c60
Boglarka Molnar <114649140+boglarkaa@users.noreply.github.com>
2023-01-14 21:24:21 +0200

    Add files via upload

---
+++ Models/ReviewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProiectFinal.Models
+{
+    public class ReviewModel
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public int NumberOfStars { get; set; }
+        public string Message { get; set; }
+
+    }
+
+    public class MainDBContext : DbContext
+    {
+        public DbSet <ReviewModel> Reviews { get; set; }
+    }
+
+}
